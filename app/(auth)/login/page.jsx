b04c8e3c579a8204940d1cef63c640a88d8b4223
Amirsaman <gharechahi72@gmail.com>
2025-08-14 13:37:57 +0330

    create cookie for login token and redirect to userPanel

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,11 +1,12 @@
+import { loginAction } from '@/actions/authActions';
 import ToggleButton from '@/components/partials/ToggleButton';
 import React from 'react';
 
 const Login = () => {
     return (
         <div className='flex justify-center items-center h-full w-full px-2 lg:px-10'>
-            <form className='w-full text-gray-600 bg-[#75b5e6] backdrop-blur-lg p-3 rounded-lg shadow mb-20'>
-                <h1 className='text-center my-5'>فرم ورود</h1>
+            <form action={loginAction} className='w-full text-gray-600 bg-[#a3cbe9] backdrop-blur-lg p-3 rounded-lg shadow mb-20'>
+                <h1 className='text-center my-5'>ورود</h1>
                 <div>
                     <label>شماره موبایل:</label>
                     <input name="phone" type='number' className='mt-2 px-4 w-full border-gray-300 border-2 rounded-full h-12 bg-[#367db4] focus:outline-blue-300 text-sky-50' />
@@ -15,11 +16,11 @@ const Login = () => {
                     <input name="password" type='password' className='mt-2 px-4 w-full border-gray-300 border-2 rounded-full h-12 bg-[#367db4] focus:outline-blue-300 text-sky-50' />
                 </div>
                 <div className='mt-5'>
-                    <ToggleButton name="remember" title="مرا بخاطر بسپار" />
+                    <ToggleButton value={1} name="remember" title="مرا بخاطر بسپار" />
                 </div>
 
                 <div className='my-6 px-4'>
-                    <button className='rounded-full w-full bg-[#2971b9] h-12 text-white'>ورود</button>
+                    <button className='rounded-full w-full bg-[#2874c1] h-12 text-white hover:bg-[#2f74ff] cursor-pointer transition-all'>ورود</button>
                 </div>
             </form>
         </div>
--- app/(main)/usersPanel/page.jsx
+++ app/(main)/usersPanel/page.jsx
@@ -1,8 +1,8 @@
 'use client'
 import { GrUserExpert } from "react-icons/gr";
-import CustomButton from '@/components/CustomButton';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import CustomButton from "@/components/partials/CustomButton";
 
 const Posts = () => {
     const router = useRouter()
@@ -10,7 +10,7 @@ const Posts = () => {
         <div className='flex flex-col justify-center items-center h-full'>
             <GrUserExpert className="size-20"/>
             <h1 className='my-5'>صفحه کاربر</h1>
-            <CustomButton className='bg-gray-500 w-64' onClick={()=>router.push("/")}>خروج</CustomButton>
+            <CustomButton className='bg-gray-500 hover:bg-gray-600 w-64 cursor-pointer' onClick={()=>router.push("/")}>خروج</CustomButton>
         </div>
     );
 }
