06b2ac38f4eb5999fc62acadbb95dc11b3aff6c8
Amirsaman <gharechahi72@gmail.com>
2025-08-15 22:50:45 +0330

    validation in server actions with zod

--- actions/authActions.js
+++ actions/authActions.js
@@ -3,20 +3,37 @@
 import axios from "axios"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import { z } from "zod"
 
-export const loginAction = async (prevState, formData) => {
+const validationSchema = z.object({
+    phone: z.string().min(1, "ورود اطلاعات این فیلد اجباری است") ,
+    password: z.string().min(1, "ورود اطلاعات این فیلد اجباری است")
+})
 
-    const phone = formData.get('phone')
-    const password = formData.get('password')
-    const remember = formData.get('remember') || 0
-    const res = await axios.post('https://ecomadminapi.azhadev.ir/api/auth/login', { phone, password, remember })
+export const loginAction = async (prevState,formData) => {
+    const phone = formData.get("phone")
+    const password = formData.get("password")
+    const remember = formData.get("remember") || 0
 
+    const params = { phone, password, remember }
+
+    const validationFields = validationSchema.safeParse(params)
+
+    if (!validationFields.success) {
+        const response = {
+            errors: validationFields.error?.flatten().fieldErrors,
+            success: false
+        }
+
+        return response
+    }
+
+    const res = await axios.post("https://ecomadminapi.azhadev.ir/api/auth/login", params)
     if (res.status === 200) {
         const token = res.data.token
-        const cookieStore = await cookies()
-        cookieStore.set("loginToken", token)
-        redirect('/usersPanel')
-    } else {
-        return {error: 'در نوشتن اطلاعات دقت کنید', success: false}
+        cookies().set("loginToken", token)
+        redirect("/userpanel")
+    }else{
+        return {error:"Unauthorized", success: false}
     }
 }
--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -6,8 +6,9 @@ import ToggleButton from '@/components/partials/ToggleButton';
 import React, { useActionState } from 'react';
 
 const Login = () => {
-    const loginActionWithParams = loginAction.bind(null, 'param1', 'param2')
-    const [state, formAction, pending] = useActionState(loginAction, {error: '', success: false})
+    const [state, formAction, pending] = useActionState(loginAction, { error: '', success: false })
+    console.log(state);
+
 
     return (
         <div className='flex justify-center items-center h-full w-full px-2 lg:px-10'>
@@ -17,10 +18,12 @@ const Login = () => {
                 <div>
                     <label>شماره موبایل:</label>
                     <input name="phone" type='number' className='mt-2 px-4 w-full border-gray-300 border-2 rounded-full h-12 bg-[#367db4] focus:outline-blue-300 text-sky-50' />
+                    <small className='text-red-600 block'>{state.errors?.phone?.at(0) || ''}</small>
                 </div>
                 <div className='mt-5'>
                     <label>رمز عبور:</label>
                     <input name="password" type='password' className='mt-2 px-4 w-full border-gray-300 border-2 rounded-full h-12 bg-[#367db4] focus:outline-blue-300 text-sky-50' />
+                    <small className='text-red-600 block'>{state.errors?.password?.at(0) || ''}</small>
                 </div>
                 <div className='mt-5'>
                     <ToggleButton value={1} name="remember" title="مرا بخاطر بسپار" />
